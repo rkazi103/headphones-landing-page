d20d758cecdfeee3c83f8c592b83188123b97e71
Rayan Kazi <rayan.quack.you@gmail.com>
2021-12-20 20:19:47 -0700

    finished header and navbar

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+const navMenu = document.getElementById("nav-menu");
+const navToggle = document.getElementById("nav-toggle");
+const navClose = document.getElementById("nav-close");
+
+if (navToggle)
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+
+if (navClose)
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+
+const navLink = document.querySelectorAll(".nav__link");
+
+navLink.forEach((element) =>
+  element.addEventListener("click", () => {
+    const navMenu = document.getElementById("nav-menu");
+    navMenu.classList.remove("show-menu");
+  })
+);
